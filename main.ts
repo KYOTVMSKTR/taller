2c868e51c586d704843de918a46d913e972401e5
KYOTVMSKTR <117926796+KYOTVMSKTR@users.noreply.github.com>
2023-08-18 10:17:42 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
 basic.forever(function () {
     if (input.lightLevel() > 100) {
-        basic.showIcon(IconNames.Sad)
-    } else {
         basic.showIcon(IconNames.Heart)
+        basic.pause(1000)
         basic.showIcon(IconNames.SmallHeart)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(1000)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(1000)
     }
 })
